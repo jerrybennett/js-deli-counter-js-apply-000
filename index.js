7e25071ca52d8df9bd397126732721ba4a99f5fa
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 17:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber( katzDeliLine, name ) {
   // adds name to end of katzDeliLine array
-  katzDeliLine.push(name);
+  katzDeliLine.push( name );
   // gets the length of katzDeliLine after a name has been added
   var num = katzDeliLine.length;
   // returns a welcome message that includes the current persons name and their number
@@ -8,15 +8,17 @@ function takeANumber( katzDeliLine, name ) {
 }
 
 function nowServing( katzDeliLine ) {
-  if( katzDeliLine.length === 0 ) {
+  if ( katzDeliLine.length === 0 ) {
     // returns the line is empty when no one is on line
-    return `There is nobody waiting to be served!`
+    return `There is nobody waiting to be served!`;
   } else {
     // returns an announcement about the person it is serving (the first name in the array)
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine( line ) {
-  if( )
+  if ( line.length === 0 ) {
+    return `The line is currently empty.`;
+  }
 }
