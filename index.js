c3946825504cddf8df45a7c4763a223bea4767d7
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-03 17:08:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function takeANumber( katzDeliLine, name ) {
   // gets the length of katzDeliLine after a name has been added
   var num = katzDeliLine.length;
   // returns a welcome message that includes the current persons name and their number
-  return `Welcome, ${name}. You are number ${num} in line.`
+  return `Welcome, ${name}. You are number ${num} in line.`;
+}
+
+function nowServing( katzDeliLine ) {
+  if( katzDeliLine.length === 0 ) {
+    
+    return `There is nobody waiting to be served!`
+  }
 }
